816d2f651c68a87637f9c03e2cb94f27c86154e8
Kevin Wong <wertykevin91@gmail.com>
2018-09-25 22:23:00 +0800

    fixed deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
-var cosToken = artifacts.require('./BulkToken.sol');
+var bulkToken = artifacts.require('./BulkToken.sol');
 var safeMath = artifacts.require('./SafeMath.sol');
+var bulkContract = artifacts.require('./BulkContract.sol');
 
 module.exports = function(deployer, network, accounts){
-    var tokenInstance;
     deployer.deploy(safeMath);
-    deployer.link(safeMath, cosToken);
-    deployer.deploy(cosToken);
+    deployer.link(safeMath, bulkToken);
+    deployer.deploy(bulkToken).then(function(){
+        deployer.deploy(bulkContract, bulkToken.address);
+    });
 }
